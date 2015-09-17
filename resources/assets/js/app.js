212354f5f0b03140cc6737fa73092a3b85339048
Daniel Morgan <me@danielmorgan.co.uk>
2015-09-17 21:42:13 +0100

    Set up browserify

---
+++ resources/assets/js/app.js
@@ -0,0 +1,5 @@
+'use strict';
+window._ = require('lodash');
+window.Vue = require('vue');
+window.$ = window.jQuery = require('jquery');
+require('bootstrap-sass/assets/javascripts/bootstrap');
--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -26,5 +26,6 @@
             </div>
         </div>
 
+        <script src="js/app.js"></script>
     </body>
 </html>
