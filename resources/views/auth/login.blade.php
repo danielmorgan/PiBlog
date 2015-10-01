d2f868915d98cab0de16c94d19a1d57ae1d153c8
Daniel Morgan <daniel.morgan@nublue.co.uk>
2015-10-01 12:35:28 +0100

    Further styling changes to login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,20 +4,25 @@
 
 @section('content')
 <div id="login" class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-	<h1>Login</h1>
+    <h1>Login</h1>
 
-	<form method="POST" action="/login">
-	    {!! csrf_field() !!}
+    <form method="POST" action="/login">
+        {!! csrf_field() !!}
 
-	    <div class="form-group">
-	    	<input type="text" name="email" placeholder="Email" value="{{ old('email') }}" class="col-xs-6">
-	    	<input type="password" name="password" placeholder="Password" class="col-xs-6">
-    	</div>
-
-	    <div class="form-group">
-	    	<label class="remember col-xs-6" for="remember"><input type="checkbox" name="remember" id="remember"> Remember Me</label>
-	    	<input type="submit" value="Login" class="btn btn-primary col-xs-6">
-    	</div>
-	</form>
+        <div class="row">
+            <div class="col-sm-6">
+                <input type="text" name="email" placeholder="Email" value="{{ old('email') }}">
+            </div>
+            <div class="col-sm-6">
+                <input type="password" name="password" placeholder="Password">
+            </div>
+            <div class="col-sm-6">
+                <label class="remember" for="remember"><input type="checkbox" name="remember" id="remember"> Remember Me</label>
+            </div>
+            <div class="col-sm-6">
+                <input type="submit" value="Login" class="btn btn-primary">
+            </div>
+        </div>
+    </form>
 </div>
 @endsection
